f5db8e8f9628c3532db596171c768ba8b5edb25b
novohispano@gmail.com <novohispano@gmail.com>
2013-02-06 15:11:22 -0700

    Queue print by function fixed

--- event_reporter.rb
+++ event_reporter.rb
@@ -40,30 +40,6 @@ class EventReporter
 		end
 	end
 
-	def save(file)
-		if file == ""
-			puts "I am sorry, but you did not specify a file to save the data."
-		else
-			File.open(file, 'w') do |file|
-				file.puts "1,LAST NAME,FIRST NAME,EMAIL,ZIPCODE,CITY,STATE,ADDRESS,PHONE"
-				id = 0
-				@queue.each do |attendee|
-					id = id + 1
-					file.puts "#{id},#{attendee.last_name},#{attendee.first_name},#{attendee.email},#{attendee.zipcode},#{attendee.city},#{attendee.state},#{attendee.street},#{attendee.phone}"
-				end
-			end
-		end
-	end
-
-	def print_table(contents)
-		puts "1".ljust(10, " ") + "LAST NAME".ljust(20, " ") + "FIRST NAME".ljust(15, " ") + "EMAIL".ljust(45, " ") + "ZIPCODE".ljust(10, " ") + "CITY".ljust(25, " ") + "STATE".ljust(7, " ") + "ADDRESS".ljust(55, " ") + "PHONE".ljust(10, " ")
-		id = 0
-		@queue.each do |attendee|
-			id = id + 1
-			puts "#{id}".ljust(10) + "#{attendee.last_name}".ljust(20) + "#{attendee.first_name}"[0..10].ljust(15) + "#{attendee.email}".ljust(45) + "#{attendee.zipcode}".ljust(10) + "#{attendee.city}".ljust(25) + "#{attendee.state}".ljust(5) + "#{attendee.street}"[0..55].ljust(60) + "#{attendee.phone}".ljust(10)
-		end
-	end
-
 	def help(key)
 		if key == ""
 			@help.each { |key, value| puts "#{key.ljust(15)}: #{value}" }
@@ -87,7 +63,7 @@ class EventReporter
 			case command
 			when "quit" then puts "Exiting Event Reporter."
 			when "load" then load(@parts[1])
-			when "find" then find(@parts[1], @parts[2])
+			when "find" then find(@parts[1], @parts[2..-1].join(" "))
 			when "queue" then
 				queue_command = @parts[1]
 				process_queue_command(queue_command)
--- queue.rb
+++ queue.rb
@@ -18,12 +18,26 @@ class Queue
 		puts "The queue was cleared."
 	end
 
+	def save(file)
+		if file == ""
+			puts "I am sorry, but you did not specify a file to save the data."
+		else
+			File.open(file, 'w') do |file|
+				file.puts "1,LAST NAME,FIRST NAME,EMAIL,ZIPCODE,CITY,STATE,ADDRESS,PHONE"
+				id = 0
+				queue_data.each do |attendee|
+					id = id + 1
+					file.puts "#{id},#{attendee.last_name},#{attendee.first_name},#{attendee.email},#{attendee.zipcode},#{attendee.city},#{attendee.state},#{attendee.street},#{attendee.phone}"
+				end
+			end
+		end
+	end
+
 	def print
 		if queue_data == []
 			puts "I am sorry, you don't have any data loaded to print."
 		else
-			queue_data
-			print_table(@queue)
+			print_table(queue_data)
 		end
 	end
 
@@ -31,7 +45,7 @@ class Queue
 		if queue_data == []
 			puts "I am sorry, you don't have any data loaded to print."
 		else
-			sorted_data = queue_data.sort {|attendee1, attendee2| attendee1.send(attribute) <=>attendee2.send(attribute)}
+			sorted_data = queue_data.sort {|attendee1, attendee2| attendee1.send(attribute) <=> attendee2.send(attribute)}
 			print_table(sorted_data)
 		end
 	end
@@ -39,7 +53,7 @@ class Queue
 	def print_table(contents)
 		puts "1".ljust(10, " ") + "LAST NAME".ljust(20, " ") + "FIRST NAME".ljust(15, " ") + "EMAIL".ljust(45, " ") + "ZIPCODE".ljust(10, " ") + "CITY".ljust(25, " ") + "STATE".ljust(7, " ") + "ADDRESS".ljust(55, " ") + "PHONE".ljust(10, " ")
 		id = 0
-		queue_data.each do |attendee|
+		contents.each do |attendee|
 			id = id + 1
 			puts "#{id}".ljust(10) + "#{attendee.last_name}".ljust(20) + "#{attendee.first_name}"[0..10].ljust(15) + "#{attendee.email}".ljust(45) + "#{attendee.zipcode}".ljust(10) + "#{attendee.city}".ljust(25) + "#{attendee.state}".ljust(5) + "#{attendee.street}"[0..55].ljust(60) + "#{attendee.phone}".ljust(10)
 		end
